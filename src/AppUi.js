20a95b9cc4bfbec28cbc6de813804953b60c40db
Manuel <25lopezm25@gmail.com>
2022-10-26 05:46:42 -0600

    se separo la logica de la app de su interfaz

---
+++ src/AppUi.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+// components
+import { Quote } from "./Components/Quote/Quote";
+import { Todo } from "./Components/Todo/Todo";
+import { TodoTab } from "./Components/TodoTab/TodoTab";
+import { Modal } from "./Components/Modal/Modal";
+import { TodoForm } from "./Components/TodoForm/TodoForm";
+import { CreateTodoButton } from "./Components/CreateTodoButton/CreateTodoButton";
+
+//styles
+import "./App.css";
+import { ReactComponent as AppLogo } from "./Assets/Icons/AppLogo.svg";
+
+function AppUi(props) {
+  return (
+    <React.Fragment>
+      <header className="app-header">
+        <h1 className="app-header__title">
+          Todo App <AppLogo className="app-header-logo" />
+        </h1>
+        <Quote></Quote>
+      </header>
+      <main className="todos-section">
+        <p className="todos-section__stats">
+          You have completed {0} of {0} todos
+        </p>
+        <section className="todos-section__main">
+          <div className="todos-section__main__tabs-container">
+            <TodoTab id="tab-all" categoryName="All" amount={0} />
+            <TodoTab id="tab-pending" categoryName="Pending" amount={0} />
+            <TodoTab id="tab-done" categoryName="Done" amount={0} />
+          </div>
+          <div className="todos-container">
+            <Todo id={1} completed todoText={"hacer los diseños"}></Todo>
+            <Todo id={2} todoText={"implementar la app"}></Todo>
+          </div>
+        </section>
+        <CreateTodoButton></CreateTodoButton>
+      </main>
+      <Modal containerId="modal">
+        <TodoForm placeholder="insert text here" />
+      </Modal>
+    </React.Fragment>
+  );
+}
+
+export { AppUi };

c3d86c1b8bc8d808f3bbc916b4b131749807ee47
Manuel <25lopezm25@gmail.com>
2022-10-31 16:35:20 -0700

    se implemento el componenete que trae citas famosas

--- src/Components/Quote/Quote.js
+++ src/Components/Quote/Quote.js
@@ -1,8 +1,40 @@
+import React from "react";
+
 function Quote() {
+  const [quote, setQuote] = React.useState({
+    content: "",
+    author: "",
+    visible: false,
+  });
+
+  let quoteValuesClass = "quote__values-container--hide";
+
+  const quoteApiUrl =
+    "https://api.quotable.io/random?tags=famous-quotes&maxLength=50";
+  const fetchData = async (url) => {
+    const data = await fetch(url);
+    const value = await data.json();
+    setQuote({
+      content: `'${value.content}'`,
+      author: `- ${value.author}`,
+      visible: true,
+    });
+  };
+
+  React.useEffect(() => {
+    fetchData(quoteApiUrl);
+  }, []);
+
+  if (quote.visible) {
+    quoteValuesClass = "quote__values-container";
+  }
+
   return (
     <div className="quote">
-      <p className="quote__text">{"'today's quote'"}</p>
-      <p className="quote__author">{"-me"}</p>
+      <div className={quoteValuesClass}>
+        <p className="quote__text">{quote.content}</p>
+        <p className="quote__author">{quote.author}</p>
+      </div>
     </div>
   );
 }
